df71588ce4e8583f0ff52a5c67737a006a11bbf5
RODRIGO MOTOLLO SIQUEIRA <rodrigo.motollo@bgp.local>
2024-06-25 19:11:54 -0300

    Adicionando variavel de ambiente

--- index.mjs
+++ index.mjs
@@ -1,13 +1,14 @@
 import { log } from "./log.mjs";
 
-export const handler = async (event) => {
-    // TODO implement
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify(event),
-    };
-    log("Log de execução evento: " + response.body)
 
-    return response;
-  };
+
+export const handler = async(event) => {
+
+    log('event :' + JSON.stringify(event));
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(event),
+    };
+};
   
