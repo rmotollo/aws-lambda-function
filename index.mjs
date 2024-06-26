9b31a167714b4aec92cff291fccca9d6f09fecec
RODRIGO MOTOLLO SIQUEIRA <rodrigo.motollo@bgp.local>
2024-06-26 14:41:13 -0300

    Corrigindo erro no código

--- index.mjs
+++ index.mjs
@@ -6,8 +6,9 @@ const s3Client = new S3({region: 'us-east-1'})
 export const handler = async(event) => {
 
     const record = event.Records[0]
-    const Bucket = event.s3.bucket.name
-    const Key = event.s3.object.key
+    const Bucket = record.s3.bucket.name
+    const Key = record.s3.object.key
+
     const getObjectResult = await s3Client.getObject({
       Bucket,
       Key
